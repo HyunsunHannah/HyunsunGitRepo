a57d6449417bfde133132ccef0f0ddc32502406f
Hyunsun Shin <onewhannah@gmail.com>
2021-06-25 00:56:15 -0400

    first release

---
+++ Desktop/COMP229/midterm/server/config/db.js
@@ -0,0 +1,10 @@
+/**
+ * Add your code here to fix URI connection
+ * 
+ */
+
+ module.exports =
+ {
+    "URI": "mongodb://localhost/book_store"
+   // "URI": "mongodb+srv://comp229midterm.kxzbi.mongodb.net/comp229midterm"
+ }
